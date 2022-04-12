5ab05a25fc3b9436b69c4a401f793e34419d4eff
Rigras12 <69604116+Rigras12@users.noreply.github.com>
2022-04-12 14:51:31 -0500

    taller 1 completado

--- Taller1/main.ts
+++ Taller1/main.ts
@@ -1,4 +1,38 @@
 import {Serie} from "./serie.js";
 import {series}from "./data.js";
 console.log(series)
+let seriesTable : HTMLElement =document.getElementById("series")!;
+let divBody:HTMLElement=document.getElementById("tabla")!;
+
+mostrarSeries(series);
+mostrarPromedio(series);
+
+
+function mostrarSeries(seriesx: Serie[]){
+    let seriesBody : HTMLElement =document.createElement("tbody");
+    for(let seriex of seriesx ){
+        let trElement:HTMLElement = document.createElement("tr");
+        trElement.innerHTML=`<td id="bold">${seriex.numero}</td>
+        <td> <a href="${seriex.paginaWeb}">${seriex.nombre}</a> </td>
+        <td> ${seriex.canal} </td>
+        <td> ${seriex.seasons} </td>`
+        seriesBody.appendChild(trElement);
+
+    }
+    seriesTable.appendChild(seriesBody)
+}
+
+function mostrarPromedio(seriesx:Serie[]){
+    let seriesPromedio : HTMLElement =document.createElement("p");
+    let contador : number =0;
+    let index:number=0;
+    for(let seriex of seriesx){
+        contador+=seriex.seasons;
+        index+=1;
+
+    }
+    let promedio=contador/index;
+    seriesPromedio.innerHTML=`Seasons average : ${promedio}`
+    divBody.appendChild(seriesPromedio);
+}
 
--- Taller1/scripts/main.js
+++ Taller1/scripts/main.js
@@ -1,2 +1,29 @@
 import { series } from "./data.js";
 console.log(series);
+var seriesTable = document.getElementById("series");
+var divBody = document.getElementById("tabla");
+mostrarSeries(series);
+mostrarPromedio(series);
+function mostrarSeries(seriesx) {
+    var seriesBody = document.createElement("tbody");
+    for (var _i = 0, seriesx_1 = seriesx; _i < seriesx_1.length; _i++) {
+        var seriex = seriesx_1[_i];
+        var trElement = document.createElement("tr");
+        trElement.innerHTML = "<td id=\"bold\">".concat(seriex.numero, "</td>\n        <td> <a href=\"").concat(seriex.paginaWeb, "\">").concat(seriex.nombre, "</a> </td>\n        <td> ").concat(seriex.canal, " </td>\n        <td> ").concat(seriex.seasons, " </td>");
+        seriesBody.appendChild(trElement);
+    }
+    seriesTable.appendChild(seriesBody);
+}
+function mostrarPromedio(seriesx) {
+    var seriesPromedio = document.createElement("p");
+    var contador = 0;
+    var index = 0;
+    for (var _i = 0, seriesx_2 = seriesx; _i < seriesx_2.length; _i++) {
+        var seriex = seriesx_2[_i];
+        contador += seriex.seasons;
+        index += 1;
+    }
+    var promedio = contador / index;
+    seriesPromedio.innerHTML = "Seasons average : ".concat(promedio);
+    divBody.appendChild(seriesPromedio);
+}
